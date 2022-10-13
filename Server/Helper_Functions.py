dbbce280eaba8e4258f1bfda777ca076fb394a7b
BensonCai <bensonca@buffalo.edu>
2022-10-13 17:48:08 -0400

    LoginPage

--- Server/Helper_Functions.py
+++ Server/Helper_Functions.py
@@ -1,28 +1,15 @@
 import socketserver
 
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         retrieved = self.request.recv(2048).strip().decode().lower()
 
+
 # <----------------------------> #
 # Return HTML, CSS & JS if there #
 # <----------------------------> #
-def homepage():
-    HTML = "HTTP/1.1 200 OK\r\n" \
-           "X-Content-Type-Options: nosniff" \
-           "\r\nContent-Type: text/html; charset=UTF-8" \
-           "\r\nContent-Length: {}\r\n\r\n{}".format(len(read("Server/Styles/App_Layout.html")), read("Server/Styles/App_Layout.html")).encode()
-    return HTML
-
-def css():
-    CSS = "HTTP/1.1 200 OK\r\n" \
-          "X-Content-Type-Options: nosniff" \
-          "\r\nContent-Type: text/css; charset=UTF-8" \
-          "\r\nContent-Length: {}\r\n\r\n{}".format(
-        len(read("Server/Styles/Background.css")),read("Server/Styles/Background.css")).encode()
-    return CSS
-
 # <-------> #
 # Read File #
 # <-------> #
@@ -34,3 +21,21 @@ def read(filename):
     for ele in idx:
         retVal += ' ' + ele
     return retVal
+
+
+def login():
+    HTML = "HTTP/1.1 200 OK\r\n" \
+           "X-Content-Type-Options: nosniff" \
+           "\r\nContent-Type: text/html; charset=UTF-8" \
+           "\r\nContent-Length: {}\r\n\r\n{}".format(len(read("Server/Styles/login.html")),
+                                                     read("Server/Styles/login.html")).encode()
+    return HTML
+
+
+def css():
+    CSS = "HTTP/1.1 200 OK\r\n" \
+          "X-Content-Type-Options: nosniff" \
+          "\r\nContent-Type: text/css; charset=UTF-8" \
+          "\r\nContent-Length: {}\r\n\r\n{}".format(len(read("Server/Styles/Background.css")),
+                                                    read("Server/Styles/Background.css")).encode()
+    return CSS
--- main.py
+++ main.py
@@ -7,12 +7,16 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # TODO Work in Progress!
         retrieved = self.request.recv(2048).strip().decode().lower()
-        home = HelpFunc.homepage()  # Loads the HTML
+        home = HelpFunc.login()  # Loads the HTML
         css = HelpFunc.css()        # Loads the CSS
         if "get / " in retrieved:
-            self.request.sendall(home)
+            self.request.sendall(
+                "HTTP/1.1 301 Moved Permanently\r\nContent-Length: 0\r\nLocation: http://localhost:8080/login\r\n\r\n".encode()
+            )
         elif ".css" in retrieved:
             self.request.sendall(css)
+        elif "get /login" in retrieved:
+            self.request.sendall(home)
         else:
             self.request.sendall(
                 "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 31\r\n\r\n There is no content to be shown".encode()
