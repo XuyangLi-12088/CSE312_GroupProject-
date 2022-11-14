c119d4b59ea2ff5bc670b0d5c8e473e06447741c
Xuyang Li <xli256@buffalo.edu>
2022-11-13 23:19:26 -0500

    Added functionalities to "Sign_up" and "Login" Web Pages

--- Server/auth.py
+++ Server/auth.py
@@ -6,55 +6,112 @@ import functools
 mongo_client = MongoClient("mongo")
 db = mongo_client["CSE312_Final_Project"] 
 
+users_collection = db["users"]
+
+# username contians each user's username, password contians each user's password
+#users_collection.insert_one({'username': "", 'password': ""})
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c032ce37b8b5cb5f4f50e2736bae275f'
 
-@app.route('/')
+# Home Page - Shopping Web Page When User not signed in
+@app.route('/', methods=["GET", "POST"])
 def home():
+    print("Request form: " + str(request.form), flush = True)
+    print("Request method: " + str(request.method), flush = True)
+    print("Request path: " + str(request.path), flush = True)
+
     return render_template('shopping_sign_out.html')
 
+# Shopping Web Page When User loged in
+@app.route('/signed_in')
+def signed_in():
+    return render_template('shopping_sign_in.html')
+
+# User Sign Up Web Page
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+
+    print("Request form: " + str(request.form), flush = True)
+    print("Request method: " + str(request.method), flush = True)
+    print("Request path: " + str(request.path), flush = True)
+
     if (request.method == "POST"):
         error = ''
         # Get entered username and password
         get_username = request.form["username"]
         get_password = request.form["password"]
-        print(get_username)
-        print(get_password)
+        print("sign up username: " + get_username, flush = True)
+        print("sign up password: " + get_password, flush = True)
 
         # Check if the entered username and password are between 6 - 20 characters
         if (len(get_username) < 6 or len(get_username) > 20):
             error = "Entered username and password must between 6 - 20 characters"
-            #return render_template("sign_up.html")
+            flash(error)
+            return render_template("sign_up.html")
         elif (len(get_password) < 6 or len(get_password) > 20):
             error = "Entered username and password must between 6 - 20 characters"
-            #return render_template("sign_up.html")
+            flash(error)
+            return render_template("sign_up.html")
 
         # Check if the entered username exist or not
+        check_exist = list(users_collection.find({"username": get_username}, {"_id": 0}))
+        print("users in database: " + str(check_exist), flush = True)
 
-        # Store username and password into the database
+        if (len(check_exist) == 0):
+            # username not exists
+            # Store username and password into the database
+            users_collection.insert_one({"username": get_username, "password": generate_password_hash(get_password)})
+            return redirect(url_for("login"))
 
+        else:
+            # username already exists
+            error = "Username already exists, Plase choose another username"
+            flash(error)
+            return render_template("sign_up.html")
 
-        flash(error)
-
-
-    # print(request.method)
-    # print(request.path)
-    # print(request.form)
     return render_template("sign_up.html")
 
+# User Log in Web Page
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
-    #print(request.method)
-    #print(request.path)
-    #print(request.form)
-    #print(request.form["username"])
-    #print(request.form["password"])
+    print("Request form: " + str(request.form), flush = True)
+    print("Request method: " + str(request.method), flush = True)
+    print("Request path: " + str(request.path), flush = True)
+
+    if (request.method == "POST"):
+        error = ''
+        # get the login username and password
+        get_login_username = request.form["username"]
+        get_login_password = request.form["password"]
+        print("login username: " + get_login_username, flush = True)
+        print("login password: " + get_login_password, flush = True)
+
+        # check if username is in the database
+        check_exist = list(users_collection.find({"username": get_login_username}, {"_id": 0}))
+        print("users in database: " + str(check_exist), flush = True)
+
+        if (len(check_exist) == 0):
+            # username does not exist in the database
+            error = "Username does not exist!"
+            flash(error)
+            return render_template("login.html")
+        else:
+            # username exist in the database
+            # get the hashed password
+            if (check_password_hash(check_exist[0]['password'], get_login_password) == True):
+                # password matches
+                return redirect(url_for("signed_in"))
+            else:
+                error = "Wrong Password, Please try again!"
+                flash(error)
+                return render_template("login.html")
+
     return render_template("login.html")
 
+# User Post Web Page
 @app.route("/post", methods=["GET", "POST"])
 def post():
     #if (request.method == "POST"):
@@ -75,8 +132,13 @@ if __name__ == '__main__':
 
 
 
+
+
+
+# <----------------Helper Commands--------------------->
+
 # command that use flask run the server
-    # flask --app project_server.py --debug run
+    # flask --app auth.py --debug run
 
 # command that use docker run the server
     # • To run your app [and database]
@@ -87,3 +149,5 @@ if __name__ == '__main__':
         # • docker-compose up --build --force-recreate
     # • To restart the containers without rebuilding
         # • docker-compose restart
+
+# <--------------------------------------------------->
